e3763d50da3b1011eb9383c5f0079058c3f153d3
Nastya981 <nastya981@example.com>
2026-09-24 10:19:23 +0300

    Домашнее задание: платежи, сериализаторы, фильтрация

--- lms/serializers.py
+++ lms/serializers.py
@@ -10,10 +10,11 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
-        fields = ('id', 'name', 'preview', 'description', 'lessons_count')
+        fields = ('id', 'name', 'preview', 'description', 'lessons_count', 'lessons')
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
