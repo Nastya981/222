3ccc9a7611155db680239413809ea51b942282f5
Nastya981 <nastya981@example.com>
2026-09-25 10:58:00 +0300

    HW: Payment model, fixtures, PaymentViewSet with filters/ordering, lessons_count in CourseSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,14 @@
 ﻿from rest_framework import serializers
-from .models import User
+from .models import User, Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'phone', 'city', 'avatar', 'is_active')
-        read_only_fields = ('id', 'is_active')
+        fields = ('id', 'email', 'phone', 'city', 'avatar')
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ('id', 'payment_date', 'amount', 'payment_method', 'course', 'lesson')
